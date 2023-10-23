9ddc59f4bcddcb6cc03aea65f361491a286ef012
Leo Ungsten <leouuuuuuuuu@gmail.com>
2023-10-23 21:41:51 +0200

    Update footer.js

--- footer.js
+++ footer.js
@@ -2,15 +2,14 @@
   
   
 document.addEventListener("DOMContentLoaded", function () {  
-  // Create a new footer element
+
+    
 var footer = document.createElement("footer");
-    // Add content to the footer
 footer.innerHTML = "Leo Ungsten --- Na22B";
 console.log("footer.js loaded");
-    // Append the footer to the container div
 var footerContainer = document.getElementById("footerContainer");
-console.log(footerContainer); // Check if footerContainer is correctly found.
+console.log(footerContainer); 
 footerContainer.appendChild(footer);
 
 
-}); // End of DOMContentLoaded
+}); 
