1ff0c63139db82eb6312adb10269475b45fd1059
Leo Ungsten <leouuuuuuuuu@gmail.com>
2023-10-24 10:03:37 +0200

    Kommentarer till js

--- footer.js
+++ footer.js
@@ -1,15 +1,17 @@
-
-  
-  
+//Script för att importera footern automatiskt till alla sidor
+//väntar på att sidan ska laddas klart
 document.addEventListener("DOMContentLoaded", function () {  
-
-    
+//skapar footern
 var footer = document.createElement("footer");
+//lägger till texten i footern
 footer.innerHTML = "Leo Ungsten --- Na22B";
-console.log("footer.js loaded");
+//hämtar footerns container
 var footerContainer = document.getElementById("footerContainer");
+//skriver ut footerns container i konsolen för lättare felsökning
 console.log(footerContainer); 
+//lägger till footern i footerns container
 footerContainer.appendChild(footer);
-
+//skriver ut footern i konsolen för lättare felsökning
+console.log("footer.js loaded");
 
 }); 
